068f47756d30481cb2946a83bb634921a84f4f9c
Ilya <uin153974748@gmail.com>
2022-06-05 18:53:51 +0300

    implemented custom framer

--- Sources/Tied/CoAPProtocol.swift
+++ Sources/Tied/CoAPProtocol.swift
@@ -17,14 +17,45 @@ class CoAPProtocol: NWProtocolFramerImplementation {
     func stop(framer _: NWProtocolFramer.Instance) -> Bool { return true }
     func cleanup(framer _: NWProtocolFramer.Instance) {}
 
-    func handleInput(framer _: NWProtocolFramer.Instance) -> Int {
-        // TODO: implement
-        0
+    func handleInput(framer : NWProtocolFramer.Instance) -> Int {
+        while true {
+        var messageLength = 0
+        var message: CoAPMessage?
+        let parsed = framer.parseInput(minimumIncompleteLength: 4, maximumLength: Int(IPV6_MMTU)){(buffer, isComplete) -> Int in
+            guard let buffer = buffer?.withUnsafeBytes({$0}) else { return 0 }
+            do {
+            message = try CoAPMessage.with(buffer)
+            } catch {
+                return 0
+            }
+            messageLength = buffer.count
+            return buffer.count
+        }
+        guard parsed, let message = message else {
+            // Ask for one more byte if failed to parse.
+            return 1
+        }
+        
+        if framer.deliverInputNoCopy(length: messageLength, message: .init(coapMessage: message), isComplete: true) == false {
+            return 0
+        }
+        }
     }
 
-    func handleOutput(framer _: NWProtocolFramer.Instance, message _: NWProtocolFramer.Message, messageLength _: Int, isComplete _: Bool) {
-        // TODO: implement
+    func handleOutput(framer : NWProtocolFramer.Instance, message : NWProtocolFramer.Message, messageLength : Int, isComplete : Bool) {
+        if let data = try? message.coapMessage.encode() {
+        framer.writeOutput(data: data)
+        }
     }
 }
 
-extension NWProtocolFramer.Message {}
+extension NWProtocolFramer.Message {
+    convenience init(coapMessage: CoAPMessage){
+        self.init(definition: CoAPProtocol.definition)
+        self["CoAPMessage"] = coapMessage
+    }
+    
+    var coapMessage: CoAPMessage {
+        self["CoAPMessage"] as! CoAPMessage
+    }
+}
